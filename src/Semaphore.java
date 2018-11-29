ee349045261c66bae0c55e48fa21a256dee44274
IIZODIACII <amr_x14@outlook.com>
2018-11-29 20:07:30 +0200

    Added Semaphore Logic

--- src/Semaphore.java
+++ src/Semaphore.java
@@ -1,3 +1,20 @@
-public class Semaphore {
-
+class Semaphore {
+    protected int avail = 0 ;
+    protected Semaphore() { avail = 0 ; }
+    public Semaphore(int num) { avail = num; }
+    public synchronized void acquire() {
+        avail-- ;
+        if (avail < 0)
+        try {
+            wait();
+        }
+        catch( InterruptedException e ) {
+            e.printStackTrace();
+        }
+    }
+    public synchronized void release() {
+        avail++;
+        if (avail >= 0)
+            notify();
+    }
 }
