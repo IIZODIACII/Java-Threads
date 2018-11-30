e576d1997ea86b3dd05db2347000fd7632c0fc33
yabani98 <31174601+yabani98@users.noreply.github.com>
2018-11-30 17:56:24 +0200

    Update OS_Thread.java

--- src/OS_Thread.java
+++ src/OS_Thread.java
@@ -3,29 +3,42 @@ package a;
 import java.util.Random;
 
 public class OS_Thread extends Thread {
-
+public synchronized void occ(){System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Occupied");}
+public synchronized void pay(){System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Paying");}
+public synchronized void lea(){System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Leave");}
  	public void run(){
  		
  		Random rand = new Random(); 
  		int value = rand.nextInt(500-201)+201; 	
  	System.out.println(this.getName()+" "+"Arrived");	
- 	Semaphore.Wait(this);
- 	System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Occupied");
- 	try {
-        OS_Thread.sleep(500);
- 	} catch (InterruptedException e) {
+ 	synchronized (this) {
+ 		Semaphore.Wait(this);
+	} 
+ 	occ();
+ 	synchronized (this) {
+		
+	try {
+		this.wait(value); 	
+		} 
+	catch (InterruptedException e) {
 		e.printStackTrace();
 	}
+ 	}
+ 	pay();
  	
- System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Paying");	
  value = rand.nextInt(500)+1;
+ synchronized (this) {
+
  try {
-		OS_Thread.sleep(500);
+		this.wait(value);
 	} catch (InterruptedException e) {
 		e.printStackTrace();
 	}
- Semaphore.Signal();
- System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Leaving");	
+ } 
+ synchronized (this) {
+	 Semaphore.Signal(this);
+}
+ lea();	
  
  
  	}
