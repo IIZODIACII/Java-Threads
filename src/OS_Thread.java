22c9b28f67cd5473f97518323dd35e785fe481db
yabani98 <31174601+yabani98@users.noreply.github.com>
2018-11-30 16:09:32 +0200

    Update OS_Thread.java

--- src/OS_Thread.java
+++ src/OS_Thread.java
@@ -5,23 +5,26 @@ import java.util.Random;
 public class OS_Thread extends Thread {
 
  	public void run(){
+ 		
  		Random rand = new Random(); 
- 		int value = rand.nextInt(500)+1; 	
+ 		int value = rand.nextInt(500-201)+201; 	
  	System.out.println(this.getName()+" "+"Arrived");	
  	Semaphore.Wait(this);
  	System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Occupied");
  	try {
-		this.wait(value);
-	} catch (InterruptedException e) {
+        OS_Thread.sleep(500);
+ 	} catch (InterruptedException e) {
 		e.printStackTrace();
 	}
+ 	
  System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Paying");	
  value = rand.nextInt(500)+1;
  try {
-		this.wait(value);
+		OS_Thread.sleep(500);
 	} catch (InterruptedException e) {
 		e.printStackTrace();
 	}
+ Semaphore.Signal();
  System.out.println("Pumb "+(Semaphore.S+1)+":"+this.getName()+" Leaving");	
  
  
