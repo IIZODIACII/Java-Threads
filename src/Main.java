ec89a4584f14c28d7f25ee6926f9b1dc82229c19
yabani98 <31174601+yabani98@users.noreply.github.com>
2018-11-30 21:06:24 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,15 @@ public class test {
 	
 	public static void main(String[] args) {
 Scanner Input=new Scanner(System.in);
-int pumb=Input.nextInt(),num_clients=Input.nextInt();
+System.out.println("Enter The Number of Pumbs?");
+		int pumb=Input.nextInt();
+System.out.println("Enter The Number of Clients?");
+		int num_clients=Input.nextInt();
 Vector<String>Names=new Vector<String>(); 	
 Input.nextLine();
+System.out.println("Enter The Names of the Clients??");
 for(int i=0;i<num_clients;i++){
+	
 	String n=Input.nextLine();
 
 	Names.addElement(n);
