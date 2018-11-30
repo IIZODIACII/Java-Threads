c2cae60f3fd2f5712b03d12c2a4e5e18db9ea6f6
yabani98 <31174601+yabani98@users.noreply.github.com>
2018-11-30 21:56:16 +0200

    Update OS_Thread.java

--- src/OS_Thread.java
+++ src/OS_Thread.java
@@ -9,6 +9,7 @@ int current_pumb=0;
 public synchronized void occ(){System.out.println("Pumb "+current_pumb+":"+this.getName()+" Occupied");}
 public synchronized void pay(){System.out.println("Pumb "+current_pumb+":"+this.getName()+" Paying");}
 public synchronized void lea(){System.out.println("Pumb "+current_pumb+":"+this.getName()+" Leave");}
+public synchronized void sev(){System.out.println("Pumb "+current_pumb+":"+this.getName()+" Being Served");}
 
 public void run(){
 	Random rand = new Random(); 
@@ -27,7 +28,16 @@ public void run(){
 		e.printStackTrace();
 	}
  	}
-
+synchronized (this) {
+        sev();	
+       try {
+		this.wait(value); 	
+		} 
+	catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+ 	}
+ 	
  	
  	pay();
 	value = rand.nextInt(100)+1;
