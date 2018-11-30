fd1829328cffa30f0213c4f5833dfeaa7d5aac7c
yabani98 <31174601+yabani98@users.noreply.github.com>
2018-11-30 19:42:59 +0200

    Update Petrol_Station.java

--- src/Petrol_Station.java
+++ src/Petrol_Station.java
@@ -6,13 +6,14 @@ public class Petrol_Station {
 	
 private Vector<OS_Thread>Clients=new Vector<OS_Thread>();
 	
-	public Petrol_Station(int pumbs,int num_clients,Vector<String>Names) {
+	public void Start(int pumbs,int num_clients,Vector<String>Names) {
+if(pumbs==0){System.out.println("There are no Pumbs");return;}
 Semaphore.S=pumbs;
 for(int i=0;i<num_clients;i++){
 OS_Thread s=new OS_Thread();
 s.setName(Names.elementAt(i));
 Clients.addElement(s);
-//didn't start The Threads yet(OS_Thread in not complete)
+Clients.lastElement().start();
 }
 
 
